c20b65bc7dfcfb0907bdcdaabb88dcbc02b9938a
Greg <greg-olivier@users.noreply.github.com>
2018-10-03 17:07:33 +0200

    state > BurgBuild + dynamic ingredients adding

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,19 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.css';
 
 const burger = (props) => {
+    const tranformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            console.log([...Array(props.ingredients[igKey])]);
+            return [...Array(props.ingredients[igKey])].map(
+                (_, i) => {
+                    return <BurgerIngredient key={igKey+i} type={igKey} />
+                }
+            );
+        });
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {tranformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
